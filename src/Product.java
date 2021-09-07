98a79f1056b9d3db1f5b5976cfa34658318cd9e9
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-07 21:30:29 +0300

    barcode is generated and saved as csv

--- src/Product.java
+++ src/Product.java
@@ -19,30 +19,30 @@ public class Product {
 	// TEST
 	public static void main(String[] args) {
 
-		// Product p1 = new Product(245135, "Macbook Pro");
-		// Product p2 = new Product(245256, "Ipad Pro", 13000, 4100);
-		// Product p3 = new Product(224522, "Airpad", 8024, 1542);
-		// Product p4 = new Product(457334, "I phone X");
-		// Product p5 = new Product(235644, "Android");
-		// Product p6 = new Product(109942, "Monster", 18034, 8425);
+		Product p1 = new Product(245135, "Macbook Pro");
+		Product p2 = new Product(245256, "Ipad Pro", 13000, 4100);
+		Product p3 = new Product(224522, "Airpad", 8024, 1542);
+		Product p4 = new Product(457334, "I phone X");
+		Product p5 = new Product(235644, "Android");
+		Product p6 = new Product(109942, "Monster", 18034, 8425);
 
 		
 		
-		// ArrayList<Product> pList = new ArrayList<Product>();
-		// pList.add(p1);
-		// pList.add(p2);
-		// pList.add(p3);
-		// pList.add(p4);
-		// pList.add(p5);
-		// pList.add(p6);
+		ArrayList<Product> pList = new ArrayList<Product>();
+		pList.add(p1);
+		pList.add(p2);
+		pList.add(p3);
+		pList.add(p4);
+		pList.add(p5);
+		pList.add(p6);
 
-		// Product.get_information_arrList(pList);
-		// Product.save_products_csv(pList, "AppleProducts");
+		Product.get_information_arrList(pList);
+		Product.save_products_csv(pList, "AppleProducts");
 
-		// ArrayList<Product> newShipment = Product.read_products_csv("AndroidProducts.csv");
-		// Product.get_information_arrList(newShipment);
+		ArrayList<Product> newShipment = Product.read_products_csv("AndroidProducts.csv");
+		Product.get_information_arrList(newShipment);
 
-		// Product.save_products_csv(newShipment, "storedProducts");
+		Product.save_products_csv(newShipment, "storedProducts");
 
 		
 	}
@@ -89,25 +89,12 @@ public class Product {
 
 			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName + ".csv")));
 
-			bw.write("SAP Code,Label,Price,Cost,Quantitiy,Weight");
+			bw.write("SAP Code,Label,Price,Cost,Quantitiy,Weight,Barcode,Barcode Number");
 			bw.newLine();
 
 			for (Product p : products) {
-
-				bw.write(p.SAP_Code + ",");
-				bw.write(p.label + ",");
-
-				if (p.price > 0) {
-					bw.write(p.price + ",");
-				}else {
-					bw.write("0,");
-				}
-
-				if (p.cost > 0) {
-					bw.write(p.cost + ",");
-				}else {
-					bw.write("0,");
-				}
+				
+				bw.write(p.csv_string());
 				
 				bw.newLine();
 
@@ -245,20 +232,9 @@ public class Product {
 
 		try {
 
-			BufferedWriter bw = new BufferedWriter(
-					new FileWriter(new File(this.SAP_Code + "_" + this.label.replace(" ", "_") + ".csv")));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(this.SAP_Code + "_" + this.label.replace(" ", "_") + ".csv")));
 
-			bw.write("SAP CODE," + this.SAP_Code + "\n");
-			bw.write("Label," + this.label + "\n");
-
-			if (this.price > 0) {
-				bw.write("Price," + this.price + "\n");
-			}
-
-			if (this.cost > 0) {
-				bw.write("Cost," + this.cost + "\n");
-			}
-
+			bw.write(csv_string());
 			bw.close();
 
 		} catch (IOException e) {
@@ -266,6 +242,48 @@ public class Product {
 			e.printStackTrace();
 		}
 
+	}
+
+	public String csv_string(){
+
+		// "SAP Code,Label,Price,Cost,Quantitiy,Weight,Barcode,Barcode Number"
+
+		String content =  this.SAP_Code + ",";
+
+		content +=  this.label + ",";
+
+		if(this.price > 0){
+			content +=  this.price + ",";
+		}else{
+			content += "0,";
+		}
+
+		if(this.cost > 0){
+			content += this.cost + ",";
+		}else{
+			content += "0,";
+		}
+
+		if(this.quantity > 0){
+			content +=  this.quantity+ ",";
+		}else{
+			content += "-,";
+		}
+		
+		if(this.weight > 0){
+			content +=  this.weight+ ",";
+		}else{
+			content += "-,";
+		}
+		
+		// Barcode 
+		if(this.barcode != null){
+			content +=  this.barcode+ ",";
+		}else{
+			content += "-,";
+		}
+
+		return content;
 	}
 
 	public String toString() {
@@ -278,6 +296,12 @@ public class Product {
 
 		if (this.cost > 0) {
 			content += "| Cost: " + this.cost + "\n";
+		}
+
+
+		// Barcode 
+		if(this.barcode != null){
+			content += "| Barcode: " + this.barcode+ "\n";
 		}
 
 		return content;
